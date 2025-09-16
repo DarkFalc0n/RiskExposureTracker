037e14c3adceec1ff52db518d4a352f07f1e4e42
darkfalc0n <pratyayroy20@gmail.com>
2025-09-16 16:48:54 +0530

    chore: correct backend structure, remove unused frontend code and add font

--- Models/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CapstoneProject_RiskExposureTrackingAndReportingSystem.Models
-{
-    public class UserModel
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string? Role { get; set; }
-    }
-}
---
+++ api/Models/ExposureSummary.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CapstoneProject_RiskExposureTrackingAndReportingSystem.Models
+{
+    public class ExposureSummary
+    {
+        [Key]
+        public long SummaryId { get; set; }
+
+        [Required]
+        [ForeignKey("Organization")]
+        public long OrgId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Category { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal TotalAmt { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+
+        public Organizations? Organizations { get; set; }
+    }
+}
