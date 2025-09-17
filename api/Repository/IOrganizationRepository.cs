030bede40278363aafba101e21967656721f1ea5
Bratati Ghosh <ghoshbratati617@gmail.com>
2025-09-17 15:28:07 +0530

    Add files via upload

---
+++ api/Repository/IOrganizationRepository.cs
@@ -0,0 +1,12 @@
+﻿using CapstoneProject_RiskExposureTrackingAndReportingSystem.Models;
+using System.Security.Claims;
+
+namespace CapstoneProject_RiskExposureTrackingAndReportingSystem.Repositories
+{
+    public interface IOrganizationRepository
+    {
+        Task<IEnumerable<Organizations>> GetAllOrganizationsAsync();
+        Task<Organizations> GetByIdAsync(long orgId);
+        Task UpdateAsync(Organizations organization);
+    }
+}
---
+++ api/Repository/OrganizationRepository.cs
@@ -0,0 +1,35 @@
+﻿using CapstoneProject_RiskExposureTrackingAndReportingSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CapstoneProject_RiskExposureTrackingAndReportingSystem.Repositories
+{
+    public class OrganizationRepository:IOrganizationRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrganizationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Organizations>> GetAllOrganizationsAsync()
+        {
+            var orgs = await _context.Organizations.ToListAsync();
+            return orgs;
+
+        }
+
+        public async Task<Organizations> GetByIdAsync(long orgId)
+        {
+            return await _context.Organizations.FindAsync(orgId);
+        }
+
+        public async Task UpdateAsync(Organizations organization)
+        {
+            _context.Organizations.Update(organization);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
