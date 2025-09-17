34bd4a839ccbb9de0e6da1f9a0c7726c6165cc54
Gnapika Pitchika <140310527+gnapika23@users.noreply.github.com>
2025-09-17 15:23:48 +0530

    Add files via upload

---
+++ api/Repository/IRiskRepository.cs
@@ -0,0 +1,12 @@
+﻿using RiskManagement.Models;
+
+namespace RiskManagement.Repository
+{
+    public interface IRiskRepository
+    {
+        Task<Risk> AddRiskAsync(Risk risk);
+        Task<List<Risk>> GetRisksByOrgAsync(long orgId);
+        Task<Risk?> GetRiskByIdAsync(long id);
+        Task<Risk?> UpdateRiskAsync(Risk risk);
+    }
+}
