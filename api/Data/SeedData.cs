911e8fa1cb82986daeebcc2ab99e32363c477187
Bratati Ghosh <ghoshbratati617@gmail.com>
2025-09-20 19:04:46 +0530

    Add files via upload

--- api/Data/SeedData.cs
+++ api/Data/SeedData.cs
@@ -1,4 +1,36 @@
+﻿using RiskExposureTracker.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace RiskExposureTracker.Data
 {
-    public static class SeedData { }
+    public class SeedData
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+
+            
+            string[] roleNames = { "Admin", "User" };
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var adminUser = await userManager.FindByEmailAsync("admin@gmail.com");
+            if (adminUser == null)
+            {
+                adminUser = new IdentityUser { UserName = "admin@gmail.com", Email = "admin@gmail.com" };
+                await userManager.CreateAsync(adminUser, "Admin123!");  
+                await userManager.AddToRoleAsync(adminUser, "Admin");   
+            }
+        }
+    }
 }
