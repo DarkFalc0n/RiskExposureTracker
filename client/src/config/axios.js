dc84c42d0569e34e788879cab256b33d834ddea0
darkfalc0n <pratyayroy20@gmail.com>
2025-09-23 16:10:41 +0530

    fix: hardcode backend domain

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://retapi-h7eqe9b0emadd0am.canadacentral-01.azurewebsites.net/api",
   timeout: 10000,
 });
 
